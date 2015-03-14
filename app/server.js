230842fa0c1b8cf16b21f94114baec5c584e6e8d
Jeanie Otter <jeanieotter@gmail.com>
2015-03-13 17:56:36 -0700

    Server.js

---
+++ app/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var jade = require('jade');
+
+var app = express();
+
+/* set views */
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+/* serve assets */
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public/'));
+
+
+/* serves main page */
+app.get("/", function(req, res) {
+	res.render('index');
+});
+
+/* serves project pages */
+app.get("/project:id", function(req, res) {
+	res.render('projects/project' + req.params.id);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
